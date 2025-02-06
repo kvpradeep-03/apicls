0838c940fadfd3879f709d7f851762d129d7f310
kvpradeep-03 <kvpradeep60@gmail.com>
2025-02-06 13:00:01 +0000

    Ostarted Auth implementation

--- api/lib/Auth.class.php
+++ api/lib/Auth.class.php
@@ -2,62 +2,65 @@
 
 require_once($_SERVER['DOCUMENT_ROOT'].'/api/lib/Database.class.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/api/lib/User.class.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/api/lib/OAuth.class.php');
 require $_SERVER['DOCUMENT_ROOT']."/vendor/autoload.php";
-class Auth{
+class Auth
+{
     private $username;
     private $password;
     private $token;
     private $db;
     private $isTockenAuth = false;
-    private $loginToken = null;
+    private $loginTokens = null;
 
-    public function __construct($username, $password = NULL){
+    public function __construct($username, $password = null)
+    {
         $this->db = Database::getConnection();
-        if($password == NULL){
+        if($password == null) {
             //token based auth
             $this->token = $username;
             $this->isTockenAuth = true;
             //we have to validate the token
-        }else{
+        } else {
             //passpord based auth
             $this->username = $username;    //it might be username or email.
             $this->password = $password;
 
         }
 
-        if($this->isTockenAuth){
+        if($this->isTockenAuth) {
             throw new Exception("Not Implemented");
-        }else{
+        } else {
             $user = new User($this->username);
             $hash = $user->getPasswordHash();
             $this->username = $user->getUserName();
-            if(password_verify($this->password, $hash)){
-                if(!$user->isActive()){
+            if(password_verify($this->password, $hash)) {
+                if(!$user->isActive()) {
                     throw new Exception("Check your email to activate your account.");
                 }
-                $this->loginToken = $this->addSession();
-            }else{
+                $this->loginTokens = $this->addSession();
+            } else {
                 throw new Exception("Password mismatch");
             }
         }
     }
 
-    public function getAuthtoken(){
-        return $this->loginToken;
+    public function getAuthtokens()
+    {
+        return $this->loginTokens;
     }
 
-    private function addSession(){
-        $token = Auth::generateRandomHash(32);
-        $querry = "INSERT INTO `session` (`username`, `token`) VALUES ('$this->username', '$token')";  
-        if(mysqli_query($this->db, $querry)){
-            return $token;
-        }else{
-            throw new Exception("unable to create session");
-        }
+    private function addSession()
+    {   
+        $oauth = new OAuth($this->username);
+        $session = $oauth->newSession();
+        return $session;
+        
     }
 
-    public static function generateRandomHash($len){
-        $bytes = openssl_random_pseudo_bytes($len,$cstrong);
+    public static function generateRandomHash($len)
+    {
+        $bytes = openssl_random_pseudo_bytes($len, $cstrong);
         return bin2hex($bytes);
     }
 }
---
+++ api/lib/OAuth.class.php
@@ -0,0 +1,62 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/api/lib/Auth.class.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/api/lib/User.class.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/api/lib/Database.class.php');
+
+class OAuth{
+    private $db;
+    private $username;
+    private $access_token;
+    private $refresh_token;
+    private $valid_for = 7200;
+
+    public function __construct($username, $refresh_token = NULL){
+        $this->refresh_token = $refresh_token;
+        $this->db = Database::getConnection();;
+        $this->username = $username;
+        $u = new User($username);
+
+    }
+
+    public function newSession($valid_for = 7200){
+        $this->valid_for = $valid_for;
+        $this->access_token = Auth::generateRandomHash(32);
+        $this->refresh_token = Auth::generateRandomHash(32);
+        $query = "INSERT INTO `session` (`username`, `access_token`, `refresh_token`, `valid_for`, `reference_token`) 
+                   VALUES ('$this->username', '$this->access_token', '$this->refresh_token', '$this->valid_for', 'auth_grant')";
+        if(mysqli_query($this->db, $query)) {
+            return array(
+                "access_token" => $this->access_token,
+                "valid_for" => $this->valid_for,
+                "refresh_token" => $this->refresh_token,
+                "type" => "api"
+            );
+        } else {
+            throw new Exception("unable to create session");
+        }
+    }
+
+    public function refreshAccess(){
+        if($this->refresh_token){
+            $query = "SELECT * FROM `session` WHERE `refresh_token` = '$this->refresh_token'";
+            $result = mysqli_query($this->db, $query);
+            if($result){
+                $data = mysqli_fetch_assoc($result);
+                if($data['valid'] == 1){
+
+                }else{
+                    throw new Exception("Expired token");
+                }
+
+            }else{
+                throw new Exception("Invalid request");
+            }
+        }
+    }
+}
+
+
+
+
+
+?>
